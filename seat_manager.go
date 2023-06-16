3632f22a3f1b0aca9feeaa940cd74ce58ecf1d77
Jeffrey Yang <jeffrey@hacknext.io>
2023-06-16 19:19:47 +0800

    add seat manager logics

---
+++ seat_manager.go
@@ -0,0 +1,228 @@
+package pokercompetition
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/weedbox/pokertable"
+	"github.com/weedbox/pokertablebalancer"
+	"github.com/weedbox/pokertablebalancer/psae"
+)
+
+func (ce *competitionEngine) SetSeatManager(seatManager pokertablebalancer.SeatManager) {
+	ce.seatManager = seatManager
+}
+
+/*
+	AutoCreateTable 拆併桌自動開桌
+	  - 適用時機: 拆併桌自動觸發
+*/
+func (ce *competitionEngine) AutoCreateTable(competitionID string) (*pokertablebalancer.CreateTableResp, error) {
+	competition, exist := ce.competitionMap[competitionID]
+	if !exist {
+		return nil, ErrCompetitionNotFound
+	}
+
+	randomString := func(size int) string {
+		source := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+		sourceRune := []rune(source)
+		builder := make([]rune, size)
+		for i := range builder {
+			builder[i] = sourceRune[rand.Intn(len(sourceRune))]
+		}
+		return string(builder)
+	}
+
+	count := len(competition.State.Tables)
+	code := fmt.Sprintf("%05d", count+1)
+	tableSetting := TableSetting{
+		ShortID:        randomString(6),
+		Code:           code,
+		Name:           code,
+		InvitationCode: "",
+		JoinPlayers:    []JoinPlayer{},
+	}
+	tableID, err := ce.addCompetitionTable(competition, tableSetting)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pokertablebalancer.CreateTableResp{
+		Success: true,
+		TableId: tableID,
+	}, nil
+}
+
+/*
+	AutoCloseTable 拆併桌自動關桌
+	  - 適用時機: 拆併桌自動觸發
+*/
+func (ce *competitionEngine) AutoCloseTable(competitionID string, tableID string) (*pokertablebalancer.CloseTableResp, error) {
+	_, exist := ce.competitionMap[competitionID]
+	if !exist {
+		return nil, ErrCompetitionNotFound
+	}
+
+	if err := ce.tableEngine.DeleteTable(tableID); err != nil {
+		return nil, err
+	}
+
+	return &pokertablebalancer.CloseTableResp{
+		Success: true,
+	}, nil
+}
+
+/*
+	AutoCloseTable 拆併桌家自動入桌
+	  - 適用時機: 拆併桌自動觸發
+*/
+func (ce *competitionEngine) AutoJoinTable(competitionID string, entries []*pokertablebalancer.TableEntry) (*pokertablebalancer.JoinTableResp, error) {
+	competition, exist := ce.competitionMap[competitionID]
+	if !exist {
+		return nil, ErrCompetitionNotFound
+	}
+
+	for _, entry := range entries {
+		playerIdx := ce.playerCacheData[entry.PlayerId].PlayerIdx
+		redeemChips := competition.State.Players[playerIdx].Chips
+
+		jp := JoinPlayer{
+			PlayerID:    entry.PlayerId,
+			RedeemChips: redeemChips,
+		}
+		if err := ce.PlayerJoin(competitionID, entry.TableId, jp); err != nil {
+			return nil, err
+		}
+	}
+
+	return &pokertablebalancer.JoinTableResp{
+		Success: true,
+	}, nil
+}
+
+func (ce *competitionEngine) activateSeatManager(competitionID string, meta CompetitionMeta) {
+	if ce.seatManager == nil {
+		return
+	}
+
+	g := psae.NewGame()
+	g.MaxPlayersPerTable = meta.TableMaxSeatCount
+	g.MinInitialPlayers = meta.TableMinPlayerCount
+	g.TableLimit = 9999 // TODO: ask for max value
+	ce.seatManager.RegisterCompetition(competitionID, g)
+}
+
+func (ce *competitionEngine) deactivateSeatManager(competitionID string) {
+	if ce.seatManager == nil {
+		return
+	}
+
+	ce.seatManager.UnregisterCompetition(competitionID)
+}
+
+func (ce *competitionEngine) seatManagerJoinPlayer(competitionID string, playerIDs []string) {
+	if ce.seatManager == nil {
+		return
+	}
+
+	c := ce.seatManager.GetCompetition(competitionID)
+
+	for _, pId := range playerIDs {
+		c.Join(pId)
+	}
+}
+
+func (ce *competitionEngine) seatManagerUpdateTable(competitionID string, table *pokertable.Table, currentPlayerIDs []string) (bool, error) {
+	if ce.seatManager == nil {
+		return false, nil
+	}
+
+	c := ce.seatManager.GetCompetition(competitionID)
+
+	ts, err := c.GetTableState(table.ID)
+	if err != nil {
+		return false, nil
+	}
+
+	// utgIndex 預設是 dealerIndex
+	currUGSeat := table.State.CurrentDealerSeat
+	if len(table.State.GamePlayerIndexes) > 4 {
+		// 4 人以上才有 utg
+		ugPlayerIdx := table.State.GamePlayerIndexes[3]
+		currUGSeat = table.State.PlayerStates[ugPlayerIdx].Seat
+	}
+
+	tableSeats := 0
+	for _, playerIdx := range table.State.GamePlayerIndexes {
+		tableSeats += 1 << table.State.PlayerStates[playerIdx].Seat
+	}
+
+	ts.LastGameID = table.State.GameState.GameID
+	ts.AvailableSeats = ce.calcAvailableSeats(tableSeats, ts.TotalSeats, table.State.CurrentDealerSeat, currUGSeat)
+
+	// 更新玩家列表
+	ts.Players = make(map[string]*psae.Player)
+	for _, pId := range currentPlayerIDs {
+		ts.Players[pId] = &psae.Player{
+			ID: pId,
+		}
+	}
+
+	updated, err := c.UpdateTable(ts)
+	if err != nil {
+		return false, nil
+	}
+
+	isSuspended := false
+	if updated.Status == psae.TableStatus_Broken {
+		// 將被拆桌，必須停止繼續下一手
+		isSuspended = true
+	}
+
+	return isSuspended, nil
+}
+
+func (ce *competitionEngine) seatManagerUpdateCompetitionStatus(competitionID string, isStoppedBuyIn bool) {
+	if ce.seatManager == nil {
+		return
+	}
+
+	c := ce.seatManager.GetCompetition(competitionID)
+
+	if isStoppedBuyIn {
+		c.DisallowRegistration()
+	}
+}
+
+func (ce *competitionEngine) calcAvailableSeats(seats int, totalSeats int, dealerIndex int, utgIndex int) int {
+
+	// 計算可入坐座位
+	seatRange := make([]int, 0)
+	if dealerIndex >= utgIndex {
+		// 列舉 UTG 到 Dealer 之間的位置
+		for i := utgIndex; i <= dealerIndex; i++ {
+			seatRange = append(seatRange, i)
+		}
+	} else {
+
+		// 列舉 UTG 到最後一個位置
+		for i := utgIndex; i < totalSeats; i++ {
+			seatRange = append(seatRange, i)
+		}
+
+		// 列舉第一個位置到 Dealer(Button) 位
+		for i := 0; i <= dealerIndex; i++ {
+			seatRange = append(seatRange, i)
+		}
+	}
+
+	// 統計空位
+	availableSeats := 0
+	for _, seatIdx := range seatRange {
+		if seats&(1<<seatIdx) == 0 {
+			availableSeats++
+		}
+	}
+
+	return availableSeats
+}
