7044c183671ae6cd39ec657d0d835fcca3ff759b
Jeffrey Yang <jeffrey@hacknext.io>
2023-06-26 22:31:00 +0800

    remove useless code

--- constant.go
+++ constant.go
@@ -1,12 +1,5 @@
 package pokercompetition
 
-import "fmt"
-
 const (
 	UnsetValue = -1
 )
-
-func LogJSON(msg string, jsonPrinter func() (string, error)) {
-	json, _ := jsonPrinter()
-	fmt.Printf("\n===== [%s] =====\n%s\n", msg, json)
-}
